8e4c41930bcaac323791f1468fd4a533efacfdd5
Sudha <sudhaismyname@gmail.com>
2021-12-16 02:29:53 +0530

    Stage 2 Completed Print your name. Introduce yourself to the bot.

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,8 +1,17 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! My name is Siri.");
         System.out.println("I was created in 2021.");
+        System.out.println("Please, remind me your name.");
+
+        // reading a name
+        String name = scanner.nextLine();
+        System.out.printf("What a great name you have, %s!", name);
     }
 }
