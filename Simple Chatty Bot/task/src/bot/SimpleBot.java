3a47fa12519f148a1cb24043065c80f792792014
Sudha <sudhaismyname@gmail.com>
2021-12-16 02:36:39 +0530

    Stage 3 Completed Guess the age. Use your knowledge of strings and numbers to make the assistant guess your age.

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -10,8 +10,20 @@ public class SimpleBot {
         System.out.println("I was created in 2021.");
         System.out.println("Please, remind me your name.");
 
-        // reading a name
         String name = scanner.nextLine();
-        System.out.printf("What a great name you have, %s!", name);
+
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+        final int remainder3 = scanner.nextInt();
+        final int remainder5 = scanner.nextInt();
+        final int remainder7 = scanner.nextInt();
+        
+        //age calculation
+        final int yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.printf("Your age is %s; that's a good time to start programming!", yourAge);
     }
 }
